c5b733a94d32a18b424a19d86edb0aabd112bdaf
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-08-18 19:19:42 +0900

    New: ci > App.php

---
+++ ci/App.php
@@ -0,0 +1,49 @@
+<?php
+/** op-unit-app:/ci/App.php
+ *
+ * @created    2024-08-18
+ * @version    1.0
+ * @package    op-unit-app
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP::Unit('CI')::Config();
+
+//	Template
+$method = 'Template';
+$args   = ['ci.txt'];
+$result = 'OK';
+$ci->Set($method, $result, $args);
+
+//	Content
+$method = 'Content';
+$args   =  null;
+$result =  null;
+$ci->Set($method, $result, $args);
+
+//	ETag
+$method = 'ETag';
+$args   =  null;
+$result =  false;
+$ci->Set($method, $result, $args);
+
+//	Title
+$method = 'Title';
+$args   = 'CI';
+$result = 'CI | The onepiece-framework app skeleton 2024';
+$ci->Set($method, $result, $args);
+
+//	...
+return $ci->Get();

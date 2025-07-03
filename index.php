a06020053adfdd08ff811cf5f06747500b21ed8c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-03 15:02:43 +0900

    Doc: index.php | PHPDoc

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
 <?php
-/**
- * op-unit-app:/index.php
+/**	op-unit-app:/index.php
  *
- * @created   2018-04-04
- * @version   1.0
- * @package   op-unit-app
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
+ * @created    2018-04-04
+ * @version    1.0
+ * @package    op-unit-app
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
  */
 
-/** Declare strict
+/**	Declare strict
  *
  */
 declare(strict_types=1);
 
-/** namespace
+/**	namespace
  *
- * @created   2019-11-21
  */
 namespace OP;
 
-/** Include
+/**	Include
  *
  */
 require_once(__DIR__.'/App.class.php');
